29857547daa45eadb749d9c1602a53c751a3dac5
magnusbil <billups.clayton@gmail.com>
2020-04-01 01:33:18 +0000

    Connect react app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,26 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import BaseRouter from './routes';
-import 'antd/dist/antd.css';
-import SiderDemo from './containers/Layout';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Router>
-          <SiderDemo>
-            <BaseRouter />
-          </SiderDemo>
-        </Router>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
