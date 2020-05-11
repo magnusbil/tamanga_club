75befda235ed4f705d802bacbfbd1969195aa359
magnusbil <billups.clayton@gmail.com>
2020-05-11 02:36:29 +0000

    add RecentBooksView

--- catalogue/api/urls.py
+++ catalogue/api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, re_path
-from .views import SeriesListView, SeriesByTitleDetailView, BooksListView, BooksBySeriesListView
+from .views import SeriesListView, SeriesByTitleDetailView, BooksListView, BooksBySeriesListView, RecentBooksView
 
 urlpatterns = [
     path('series', SeriesListView.as_view()),
     path('series/<series_title>', SeriesByTitleDetailView.as_view()),
     path('books', BooksListView.as_view()),
-    path('books/<series>', BooksBySeriesListView.as_view()),
+    path('recents', RecentBooksView.as_view()),
 ]
