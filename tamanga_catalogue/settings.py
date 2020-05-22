48e8ca353c5d209de04449b2e9d34b8d2462781e
magnusbil <billups.clayton@gmail.com>
2020-05-22 19:47:49 -0400

    AH another static file config change

--- tamanga_catalogue/settings.py
+++ tamanga_catalogue/settings.py
@@ -147,9 +147,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = [
-  os.path.join(BASE_DIR, 'build/static')
-]
+STATICFILES_DIRS = []
 MEDIA_URL= '/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 
