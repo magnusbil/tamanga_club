d1155f4ef1c4ad54f750703b79f292254c6b07aa
magnusbil <billups.clayton@gmail.com>
2020-05-22 19:36:24 -0400

    revert static files location

--- tamanga_catalogue/settings.py
+++ tamanga_catalogue/settings.py
@@ -145,9 +145,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = []
+STATICFILES_DIRS = [
+  os.path.join(BASE_DIR, 'build/static')
+]
 MEDIA_URL= '/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 
