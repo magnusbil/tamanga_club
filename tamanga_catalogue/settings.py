6e646a881fba6f80d6642956d5feab6ae4a5da08
magnusbil <billups.clayton@gmail.com>
2020-05-22 19:57:13 -0400

    I know just what do with you... REVERT

--- tamanga_catalogue/settings.py
+++ tamanga_catalogue/settings.py
@@ -147,7 +147,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = []
+STATICFILES_DIRS = [
+  os.path.join(BASE_DIR, 'build/static')
+]
 MEDIA_URL= '/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 
