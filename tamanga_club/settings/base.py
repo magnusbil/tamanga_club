f60e02a7f0f797fb0adc89e7185ed87eb397bf73
magnusbil <billups.clayton@gmail.com>
2020-06-16 22:10:10 -0400

    Remove logger

--- tamanga_club/settings/base.py
+++ tamanga_club/settings/base.py
@@ -111,19 +111,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
   os.path.join(BASE_DIR, 'build/static')
 ]
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
-        },
-    },
-}
