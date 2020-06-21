e73d01131faf9e101a1d2cee4f68c055fd8e7420
magnusbil <billups.clayton@gmail.com>
2020-06-20 23:22:08 -0400

    Add delete account form

--- src/containers/Account/SettingsPage.js
+++ src/containers/Account/SettingsPage.js
@@ -2,15 +2,50 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { deleteAccount } from '../../actions/auth';
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col, Button, Form } from 'react-bootstrap';
 
 class SettingsPage extends React.Component {
+  state = {
+    security_answer: '',
+  };
+
   static propTypes = {
     deleteAccount: PropTypes.func.isRequired,
   };
 
+  submitDelete() {
+    if (this.state.security_answer) {
+      this.props.deleteAccount(
+        this.props.user.username,
+        this.state.security_answer
+      );
+    }
+  }
+
+  onChange = (e) => this.setState({ [e.target.id]: [e.target.value] });
+
   render() {
-    return <Container></Container>;
+    return (
+      <Container>
+        <Row>
+          <Col>
+            <Form>
+              <Form.Label>Delete Account</Form.Label>
+              <Form.Group controlId="security_answer">
+                <Form.Label>
+                  {this.props.user.profile.security_answer}
+                </Form.Label>
+                <Form.Control
+                  placeholder="Answer"
+                  onChange={this.onChange}
+                ></Form.Control>
+              </Form.Group>
+              <Button onClick={this.submitDelete}></Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    );
   }
 }
 
