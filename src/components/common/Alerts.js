153440cc556b795e01e0baf3b1c874ca5dcbddae
magnusbil <billups.clayton@gmail.com>
2020-06-25 22:06:07 -0400

    Add basic tests for App, SeriesList and PollList

--- src/components/common/Alerts.js
+++ src/components/common/Alerts.js
@@ -10,7 +10,6 @@ export class Alerts extends Component {
   };
 
   componentDidUpdate(prevProps) {
-    console.log(this.props);
     const { error, alert, message } = this.props;
     if (error !== prevProps.error) {
       if (error.msg.message) alert.error(`Error: ${error.msg.message.join()}`);
